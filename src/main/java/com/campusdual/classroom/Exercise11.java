1aebde53df8dec612378809618edc0abd61c69c6
Jorge González Fernández <j.gonzalezf@udc.es>
2024-12-17 17:50:47 +0100

    Implementación de atributos y métodos en una clase

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,33 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        // Encender la TV
+        blackRemote.turnOn();
+
+        // Subir un canal
+        blackRemote.channelUp();
+
+        // Bajar un canal
+        blackRemote.channelDown();
+
+        // Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        blackRemote.channelDown();
+
+        // Subir el volumen
+        blackRemote.volumeUp();
+
+        // Bajar el volumen
+        blackRemote.volumeDown();
+
+        // Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        blackRemote.volumeDown();
+
+        // Obtener el color del mando a distancia y mostrarlo por pantalla
+        System.out.println(blackRemote.getColor());
+
+        // Apagar la TV
+        blackRemote.turnOff();
     }
 
 }
